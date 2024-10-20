713c85a9f72de44c84535f1d269222f93495c1b1
laukk96 <lau.kiaran@gmail.com>
2024-10-19 18:19:53 -0700

    added .prettierrc

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,5 @@ import autoprefixer from 'autoprefixer'
 import tailwindConfig from './src/css/tailwind.config.js'
 
 export default {
-  plugins: [tailwind(tailwindConfig), autoprefixer],
+  plugins: [tailwind(tailwindConfig), autoprefixer]
 }
